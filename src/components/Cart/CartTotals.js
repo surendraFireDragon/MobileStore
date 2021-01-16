5645e96b10b2a905220295434b457eae22331a4d
Surendra Pratap Singh <surendraps@gmail.com>
2021-01-16 18:16:51 +0530

    pp button resolved

--- src/components/Cart/CartTotals.js
+++ src/components/Cart/CartTotals.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
-import PayPalButton from './PayPalButton'
+// import PayPalButton from './PayPalButton'
 
 export default function CartTotals({value, history}) {
     const {cartSubTotal, cartTax, cartTotal, clearCart} = value;
@@ -35,7 +35,8 @@ export default function CartTotals({value, history}) {
                         </span>
                         <strong>${cartTotal}</strong>
                     </h5>
-                    <PayPalButton total={cartTotal} clearCart={clearCart} history={history}/>
+                    {/* <PayPalButton total={cartTotal} clearCart={clearCart} history={history}/> */}
+                    {/* <PayPalButton /> */}
                 </div>
             </div>
         </div>
