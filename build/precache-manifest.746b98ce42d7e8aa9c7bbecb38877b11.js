fc189d337303c6f519e6a0bd9c2d32d916b2f1bb
Surendra Pratap Singh <surendraps@gmail.com>
2021-01-16 17:40:00 +0530

    test deploy

---
+++ build/precache-manifest.746b98ce42d7e8aa9c7bbecb38877b11.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "36cbf0689515494890d0",
+    "url": "/static/css/main.ccb67815.chunk.css"
+  },
+  {
+    "revision": "36cbf0689515494890d0",
+    "url": "/static/js/main.3eebb615.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f8b92ffa6e09ba9debe1",
+    "url": "/static/css/2.5d1d5771.chunk.css"
+  },
+  {
+    "revision": "f8b92ffa6e09ba9debe1",
+    "url": "/static/js/2.610d8b0e.chunk.js"
+  },
+  {
+    "revision": "9f7e506d7455da27ce24e474c84a8e0a",
+    "url": "/static/media/logo.9f7e506d.svg"
+  },
+  {
+    "revision": "569201989a2c06dbc6821c77372ddf12",
+    "url": "/index.html"
+  }
+];
